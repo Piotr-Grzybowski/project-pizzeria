5e731d21f771cb5298a8ef769e07cdf842e29952
Piotr Grzybowski <grzybek6@gmail.com>
2020-01-16 18:51:04 +0000

    Add carousel

--- src/js/components/LandingPage.js
+++ src/js/components/LandingPage.js
@@ -1,12 +1,13 @@
+/* global Glide */
 import { templates } from '../settings.js';
 import { utils } from '../utils.js';
+
 
 export class LandingPage {
   constructor(wrapper) {
     const thisLandingPage = this;
-
-    // thisLandingPage.initCarousel();
     thisLandingPage.render(wrapper);
+    thisLandingPage.initCarousel();
   }
 
   render(wrapper) {
@@ -17,5 +18,22 @@ export class LandingPage {
     thisLandingPage.wrapper = wrapper;
 
     thisLandingPage.wrapper.appendChild(utils.createDOMFromHTML(generatedHTML));
+  }
+
+  initCarousel() {
+    const thisLandingPage = this;
+
+    const slider = thisLandingPage.wrapper.querySelector('.glide');
+    const options = {
+      type: 'carousel',
+      autoplay: 3000,
+      animationDuration: 1500,
+      startAt: 1,
+    };
+
+    if (slider) {
+      const glide = new Glide(slider, options);
+      glide.mount();
+    }
   }
 }
